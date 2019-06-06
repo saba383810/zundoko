5045d40cc36e90978b0dec63643b801c443aa718
saba <51075679+saba383810@users.noreply.github.com>
2019-06-06 23:23:35 +0900

    Update zun_doko_kiyoshi.c

--- zun_doko_kiyoshi.c
+++ zun_doko_kiyoshi.c
@@ -8,27 +8,22 @@ int main (){
 	int num,flag = 0;
 	int zun_cnt = 0;
 	char *kiyo = kiyoshi; 
+	
 	srand((unsigned int) time(NULL)); 
-
 	do{
 		num  = rand() % 2 + 1;
 		switch(num){
-		
+				
 			case 1:
 				printf("ズン\n");
 				zun_cnt++;
 				break;
-
 			case 2:
 				printf("ドコ\n");
 				if(zun_cnt == 4) flag = 1;
 				else zun_cnt = 0;
 				break;
-			
 		}
 	}while(flag != 1);
 	printf("%s\n",kiyo);
-	
-
 }
-
